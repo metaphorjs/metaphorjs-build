40fec8def28c6d4a1c87b33abe1ea77c397d452d
Johann Kuindji <kuindji@gmail.com>
2019-03-22 13:48:47 +0300

    cleanup

--- src/plugin/code/Prebuilt.js
+++ src/plugin/code/Prebuilt.js
@@ -20,41 +20,14 @@ module.exports = Base.$extend({
         host.builder._prebuilt = host.builder._prebuilt || {};
         MetaphorJs.app.prebuilt.setStorage(host.builder._prebuilt);
 
-        //host.$$observable.createEvent("collect-prebuilt", "merge");
-        //host.$$observable.createEvent("collect-prebuilt-funcs", "merge");
         host.on("prepare-prebuilt", self._prepareTemplates, self);
-        //host.on("collect-prebuilt-funcs", self._prepareFuncs, self);
     },
 
-    /*_prepareFuncs: function() {
-        return this.host.builder._prebuilt.expressionFuncs;
-    },*/
 
     _prepareTemplates: function() {
 
-        console.log("prepare templates")
-
-        /*var tpls = {},
-            opts = {},
-            cfgs = {};*/
-
         this.host.templateList.forEach(function(tpl){
-            console.log(tpl.id)
             MetaphorJs.app.prebuilt.add("templates", tpl.toString(), tpl.id);
-            /*tpls[tpl.id] = tpl.toString();
-            if (tpl.optObj) {
-                opts[tpl.id] = tpl.optObj;
-            }
-            if (tpl._prebuilt) {
-                extend(cfgs, tpl._prebuilt.configs, false, false);
-            }*/
         });
-
-        /*return {
-            templates: tpls,
-            templateOptions: opts,
-            //configs: cfgs,
-            expressionOpts: this.host.builder._prebuilt.expressionOpts
-        };*/
     }
 });
